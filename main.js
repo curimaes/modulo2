8b3f2c9330345d1df775d12f60b0d4b8b52a263d
curimaes <59623012+curimaes@users.noreply.github.com>
2020-01-30 17:17:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-document.getElementById("senate-data").innerHTML = JSON.stringify(data,null,2)
-
 const tbody= document.querySelector("tbody")
 
 var members= data.results[0].members;
 
 members.forEach(member => {
 	let row= tbody.insertRow (-1);
-		
-		if (member.middle_name == null) { 
+
+	if (member.middle_name == null) { 
 			var middle = ""
 		} else{
 			var middle = member.middle_name
 		}
-	 row.innerHTML= `<td> ${member.first_name} ${middle} ${member.last_name} </td>` 
+	let fullName=`${member.first_name} ${middle} ${member.last_name}`
+	let anchor= member.url=="" ? fullName : `<a href="${member.url}"> ${fullName} </a>`
+		
+		
+	 row.innerHTML= `<td> ${anchor} </td>` 
 	 row.innerHTML+= `<td> ${member.party} </td>`
 	 row.innerHTML+= `<td> ${member.state} </td>`
 	 row.innerHTML+= `<td> ${member.seniority} </td>`
-	 row.innerHTML+= `<td> ${member.total_votes} </td>` 
+	 row.innerHTML+= `<td> ${member.total_votes} </td>`
 })
+
+const tfoot= document.querySelector("tfoot")
